54a3ce2ae31486a1cbd951a1165f8e135287e819
Djalma Melo S. <djalmamelos@gmail.com>
2025-10-14 22:38:11 -0300

    adicionado teste de falha de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,4 +7,13 @@ describe('Login', () => {
     cy.contains('h4','Realizar Transferência').should('be.visible');
     
   })
+
+    it('Login com dados validos deve ter falha', () => {
+    cy.visit('/')
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
+    cy.get('#login-section > .btn').click()
+    // cy.contains('h4','Realizar Transferência').should('be.visible');
+    cy.get('#toast-container div.red').should('have.text', 'Erro no login. Tente novamente.');
+    })
 })
